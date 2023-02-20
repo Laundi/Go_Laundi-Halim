2afaf0e02371470a151c49e1c7ad4174cad9c49e
Laundi <Laundih@gmail.com>
2023-02-20 11:01:26 +0700

    Basic Programming

---
+++ Soal Prioritas 1/First Basic.go
@@ -0,0 +1,5 @@
+// function luas trapesium
+fuc luas trapesium (a float64, b float64, t float64) float64 {
+	return ((a=b)*t)/2
+}
+
---
+++ Soal Prioritas 1/Second Basic.go
@@ -0,0 +1,7 @@
+// function GanjilGenap
+func GanjilGenap(a int) string {
+	if a%2== 0 {
+		return "Genap"
+	}
+	return "Ganjil"
+}
